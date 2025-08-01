7b07f184309530ccebc435118a57e875e35ef7a4
Alan TS <alanedgardo971@gmail.com>
2025-07-31 23:17:22 -0600

    Añadiendo objeto

--- src/modelos/Moneda.java
+++ src/modelos/Moneda.java
@@ -1,4 +1,27 @@
 package modelos;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
+
 public class Moneda {
+    @SerializedName("base_code")
+    private String nombre;
+    @SerializedName("conversion_rates")
+    private Map<String, Double> conversiones;
+
+    public Map<String, Double> getConversiones() {
+        return conversiones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Moneda{" +
+                "conversion=" + conversiones +
+                '}';
+    }
 }
